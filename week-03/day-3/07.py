72328350ddbc83c0ab0c7682687a0cd3ad146e86
GergoV <geritwo@gmail.com>
2016-11-05 15:25:46 +0100

    Friday's exercises in progress

--- week-03/day-3/07.py
+++ week-03/day-3/07.py
@@ -1,1 +1,21 @@
 # create a function that takes a list and returns a new list with all the elements doubled
+
+class DoubleEm:
+    
+    items = []
+    
+    def __init__(self, *items):
+        self.items = items
+    
+    def show(self):
+        print("This is your list:" + str(self.items))
+        
+    def double(self):
+        for i in self.items:
+            doubleitem = i * 2
+            
+            
+        
+
+skunk = DoubleEm(4, 5, 6, 7, 8)
+skunk.show()
--- week-03/day-3/08.py
+++ week-03/day-3/08.py
@@ -4,3 +4,58 @@
 # Create a `Student` class that is the child class of `Person`
 # it should have a method to add grades
 # it should have a `salute` method that prints it's full name and the average of it's grades as well
+
+class Person:
+          
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+     
+    def greet(self):
+        print("Hi, I'm the Parent, my name is " + self.firstname + " " + self.lastname + ".")
+
+        
+class Student(Person):
+    
+    grades = []
+    gradesum = 0
+    average = 0
+    
+    def __init__(self, firstname):
+        self.firstname = firstname
+        
+    def setparent(self, obj):
+        self.parent = obj
+        self.student_lastname = self.parent.lastname
+        
+    def addgrade(self, newgrade):
+        self.grades += [newgrade]
+        print("Grade " + str(newgrade) + " added.")
+        print("These are my grades:" + str(self.grades))
+
+    def salute(self):
+        print("Hi, I'm the Student, my name is " + self.firstname + " " + self.student_lastname + ".")
+
+        # Don't divide by 0.
+        for i in self.grades:
+            self.gradesum += i
+        
+        if self.gradesum == 0:
+            print("I have no grades yet.")
+        else:
+            self.average = self.gradesum / len(self.grades)
+            print("The average of my grades is:" + str(self.average))
+        
+
+     
+Homer = Person("Homer","Simpson")
+Bart = Student("Bart")
+Bart.setparent(Homer)
+
+Homer.greet()
+Bart.salute()
+
+Bart.addgrade(5)
+Bart.addgrade(4)
+
+Bart.salute()
