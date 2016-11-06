75efeecd5fc3bd4dbb3fbaab4a942c46f7c2f4a3
GergoV <geritwo@gmail.com>
2016-11-06 22:05:43 +0100

    Diamond formula worked out.

--- week-03/day-3/11.py
+++ week-03/day-3/11.py
@@ -13,6 +13,28 @@
 #
 # It should take a number as parameter that describes how many lines the diamond has
 
+
+class drawmediamond:
+    
+    indentsinrow = 1
+    
+    def __init__(self, lines):
+        self.lines = lines
+        
+    def drawit(self):
+        for i in range(0, self.lines): 
+            if i <= self.lines // 2: # To center line, if odd no. of lines
+                aster = i * 2 + 1 # *2 + 1 to make it odd; 0 becomes 1
+            if i > self.lines // 2:
+                # n is: steps taken from x // 2 to last step
+                n = i - (self.lines - i)
+                aster = (i * 2) - (n * 2) - 1
+            indents = (self.lines * 2 - aster) // 2 - 3 # The remaining space between x(max) not filled, /2 because indent left; donno why too much
+            print(indents * " " + aster * "*")
+
+"""
+1st version: calculation + pirmitive steps
+
 class drawmediamond:
     
     indentsinrow = 1
@@ -33,7 +55,7 @@ class drawmediamond:
                 
             print(self.indentsinrow * " " + self.starsinrow * "*")
             
+"""
           
-          
-primitivediamond = drawmediamond(9)
+primitivediamond = drawmediamond(11)
 primitivediamond.drawit()
