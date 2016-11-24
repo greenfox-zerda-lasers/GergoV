6ebc935fd2c8f0ef2b88cb9bc065e62b9d0d8ddb
GergoV <geritwo@gmail.com>
2016-11-24 10:20:35 +0100

    Images loaded in view init because it's more failsafe.

--- week-06/project/view.py
+++ week-06/project/view.py
@@ -8,10 +8,18 @@ from PIL import Image, ImageTk
 class GameDisplay:
 
     def __init__(self, area_floorplan):
+
+        # Init area variables:
         self.area_floorplan = area_floorplan
         self.canvas_dimensions = [len(self.area_floorplan), len(self.area_floorplan[0])]
         self.tile_width = 72
 
+        # Init imagery:
+        self.floor_image = PhotoImage(file='./img/floor.png')
+        self.wall_image = PhotoImage(file='./img/wall.png')
+        self.hero_image = PhotoImage(file='./img/hero-down.png')
+
+        # Init canvas:
         self.root = Tk()
         self.canvas = Canvas(self.root, width=self.canvas_dimensions[1]*self.tile_width,       height=self.canvas_dimensions[0]*self.tile_width)
         self.canvas.pack()
@@ -21,19 +29,17 @@ class GameDisplay:
         pass
 
     def display_loop(self, hero_position):
+        # TODO: Add more required data as input arguments: Enemy positions(dict), Status display data(rows?)
 
         # Display background:
-        floor_image = PhotoImage(file='./img/floor.png')
-        wall_image = PhotoImage(file='./img/wall.png')
-        select_tile_pattern_display = {'0': floor_image, '1': wall_image}
+        select_tile_pattern_display = {'0': self.floor_image, '1': self.wall_image}
 
         for row in range(self.canvas_dimensions[0]):
             for column in range(self.canvas_dimensions[1]):
                 self.canvas.create_image(column*self.tile_width, row*self.tile_width, anchor=NW, image=select_tile_pattern_display[self.area_floorplan[row][column]])
 
         # Display hero:
-        hero_image = PhotoImage(file='./img/hero-down.png')
-        self.canvas.create_image(hero_position[0], hero_position[1], anchor=NW, image=hero_image)
+        self.canvas.create_image(hero_position[0], hero_position[1], anchor=NW, image=self.hero_image)
 
         # Listen keyboard input
         # TODO: self.root.bind('<KeyPress>', self.key_move_down) - bind this to root in control
