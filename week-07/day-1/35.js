47e753a82e739ac99baee1a6a50f4a4f9d1b10ff
GergoV <geritwo@gmail.com>
2016-12-05 16:49:08 +0100

    31-39.js done (and not .py like I wrongly stated in previous commit, after 4 weeks of python

---
+++ week-07/day-1/35.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var ah = ['kuty', 'cic', 'mérési hib']
+// add to all elements an 'a' on the end
+
+for (var i = 0; i < ah.length; i++) {
+  ah[i] = ah[i] + 'a'
+}
+
+console.log(ah)
---
+++ week-07/day-1/37.js
@@ -0,0 +1,20 @@
+
+'use strict';
+
+// print the even numbers till 20
+
+/*
+for (var i = 1; i <= 20; i++) {
+  if (i % 2 == 0) {
+    console.log(i)
+  }
+}
+*/
+
+var a = 0;
+while (a <= 20) {
+  a += 1;
+  if (a % 2 == 0) {
+    console.log(a);
+  }
+}
