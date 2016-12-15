da06ca0bac1112a802cd002cfd1a9f480a181651
GergoV <geritwo@gmail.com>
2016-12-15 20:51:59 +0100

    Webapp view I/O functions work. TODO: Wire it up with API I/O.

--- week-08/project/todo.js
+++ week-08/project/todo.js
@@ -3,13 +3,19 @@
 // Main interface
 
 var inputFieldAddButton = document.getElementById('add-todo-button');
+var inputFieldElement = document.getElementById('submit-todo-field');
+
+inputFieldAddButton.addEventListener('click', function () {
+  viewAddTask(inputFieldElement.value);
+});
+
 var todoList = document.querySelector('ul');
 
 // Dynamic
 var todoCheckBox = document.getElementById('list-item');
 var todoDeleteButton = document.getElementById('list-item-remove')
 
-// Server I/O
+// Server I/O - read + View: render
 
 let getList = new XMLHttpRequest();
 getList.open('GET', 'https://mysterious-dusk-8248.herokuapp.com/todos', true);
@@ -26,14 +32,18 @@ function initGetData() {
 function viewDisplayData(data) {
   data.forEach(function(e, i) {
     var itemToView = document.createElement('li');
+
     var todoLabel = document.createElement('label');
     todoLabel.setAttribute('class', 'todo-item');
     todoLabel.setAttribute('for', 'list-item-' + i);
+
     var todoText = document.createElement('p');
     todoText.textContent = e.text;
+
     var removeTodo = document.createElement('input');
     removeTodo.setAttribute('type', 'button');
     removeTodo.setAttribute('id', 'list-item-remove');
+
     var checkBox = document.createElement('input');
     checkBox.setAttribute('type', 'checkbox');
     checkBox.setAttribute('name', 'complete-item');
@@ -42,10 +52,12 @@ function viewDisplayData(data) {
     if (e.completed == false) {
       checkBox.removeAttribute('checked','checked');
     };
+
     var spriteSpan = document.createElement('span');
 
     // NOTE: Quick view for debugging API I/O
     itemToView.textContent = 'ID: ' + e.id + ', ' + e.text + ', ' + e.completed;
+
     todoList.appendChild(itemToView);
     itemToView.appendChild(todoLabel);
     todoLabel.appendChild(todoText);
@@ -53,22 +65,33 @@ function viewDisplayData(data) {
     todoLabel.appendChild(checkBox);
     todoLabel.appendChild(spriteSpan);
 
+    // Click listeners
+    spriteSpan.addEventListener('click', function() {
+      viewCheckTask(e.id, e.text);
+    });
+    todoText.addEventListener('click', function() {
+      viewCheckTask(e.id, e.text);
+    });
+    removeTodo.addEventListener('click', function() {
+      viewDeleteTask(e.id);
+    });
+
   });
 };
 
-function viewAddTask() {
-  window.alert('add');
+function viewAddTask(string) {
+  console.log('add' + string);
 };
 
 function viewCheckTask() {
-  window.alert('check');
+  console.log('check');
 };
 
 function viewDeleteTask() {
-  windows.alert('delete');
+  console.log('delete');
 };
 
-// Add, Check, Remove
+// Server I/O cont. - Add, Check, Remove
 
 function addNewTask(task){
   let postList = new XMLHttpRequest();
@@ -103,6 +126,7 @@ function deleteTask(id){
 };
 
 function updateTask(id, string){
+  // NOTE: Send without string - overwrites with ''
   let postList = new XMLHttpRequest();
   postList.open('PUT', 'https://mysterious-dusk-8248.herokuapp.com/todos/' + id, true);
 
