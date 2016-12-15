09eabf9f7afe578796d46324bee124581f385c8e
GergoV <geritwo@gmail.com>
2016-12-15 14:51:17 +0100

    Project main interface JS functions done

--- week-08/project/todo.js
+++ week-08/project/todo.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// Main interface
+
+var inputFieldAddButton = document.getElementById('add-todo-button');
+var todoList = document.querySelector('ul');
+
+// Dynamic
+var todoCheckBox = document.getElementById('list-item');
+var todoDeleteButton = document.getElementById('list-item-remove')
+
+// XHR
+
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://mysterious-dusk-8248.herokuapp.com/todos?api_key=63r17w0', true);
+xhr.send(null);
+
+xhr.onreadystatechange = initData;
