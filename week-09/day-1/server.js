c19fc79d659ee0eceabc8ad0b442c20da4354f25
GergoV <geritwo@gmail.com>
2016-12-19 13:20:04 +0100

    NPM/Node server setup started

---
+++ week-09/day-1/server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var http = require('http');
+var date = new Date();
+
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hey! How are you? The current date is: ' + date);
+});
+
+server.listen(3000, '127.0.0.1');
