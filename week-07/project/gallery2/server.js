907b7ead191a94405085597907e1a78bca998680
geritwo <geritwo@gmail.com>
2017-03-14 14:14:34 +0100

    Project files served by server.

--- week-07/project/gallery2/server.js
+++ week-07/project/gallery2/server.js
@@ -4,5 +4,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 
+// Express config
 const app = express();
+const portNum = 8080;
 app.use(bodyParser.json());
+
+app.use(express.static('app'));
+
+// Endpoints
+app.get('/files', function(req, res) {
+  res.send('You have requested file list.');
+});
+
+console.log('Server up on port ' + portNum + '.');
+app.listen(portNum);
