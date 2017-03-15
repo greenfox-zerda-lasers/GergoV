aa53aa63332741423b63999450dcdc22013f9722
geritwo <geritwo@gmail.com>
2017-03-15 17:26:35 +0100

    File parsing, loading to UI in progress.

--- week-07/project/gallery2/app/jsgallery.js
+++ week-07/project/gallery2/app/jsgallery.js
@@ -1,4 +1,37 @@
-const document = document;
+'use strict';
+
+
+// Load images and thumbnails
+
+function httpGetAsync(url, callback) {
+  const xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+      callback(xmlHttp.responseText);
+    }
+  };
+  xmlHttp.open("GET", url, true); // true for asynchronous
+  xmlHttp.send(null);
+}
+
+function renderImages(imageList) {
+  let imagesNo = imageList.length;
+  console.log(imageList); // NOTE: Debug;
+  // NOTE: imageList looks like an array tho not an array.
+  imageList.forEach( function(image, index) {
+    let currentImage = document.createElement('img');
+    currentImage.setAttribute('thumb-index', index);
+    if (index === 0) {
+      mainImage.setAttribute('style', 'background-image: url(', image, ')');
+      currentImage.setAttribute('class', 'thumbnail-active');
+    } else {
+      currentImage.setAttribute('class', 'thumbnail');
+    }
+    thumbnailBar.appendChild(currentImage);
+  });
+}
+
+httpGetAsync('http://localhost:8080/files', renderImages);
 
 // Fetch interactive elements
 
@@ -9,11 +42,9 @@ const rightBezel = document.querySelector('.main-image-bezel-right');
 const leftArrow = document.querySelector('#arrow-left');
 const rightArrow = document.querySelector('#arrow-right');
 
+const thumbnailBar = document.querySelector('.thumbnail-wrapper');
 const thumbnail = document.querySelectorAll('.thumbnail');
 const activeThumbnail = document.querySelector('.thumbnail-active');
-
-// Load images and thumbnails
-
 // Add functions to elements
 
 // Handle actions
--- week-07/project/gallery2/server.js
+++ week-07/project/gallery2/server.js
@@ -1,8 +1,12 @@
+/* eslint-env node */
+
 'use strict';
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const path = require('path');
+const p = './_data/';
 
 // Express config
 const app = express();
@@ -10,10 +14,29 @@ const portNum = 8080;
 app.use(bodyParser.json());
 
 app.use(express.static('app'));
+app.use(express.static('_data'));
 
 // Endpoints
 app.get('/files', function(req, res) {
-  res.send('You have requested file list.');
+
+  let imageList = [];
+
+  // Parse files from path
+  fs.readdir(p, function (err, files) {
+    if (err) {
+      throw err;
+    }
+
+    files.map(function addPath(file) {
+      return path.join(p, file); // Add path to filenames
+    }).filter(function filterFiles(file) {
+      return fs.statSync(file).isFile(); // Return files only
+    }).forEach(function compileArray(file) {
+      imageList.push(file); // Compile list of files in array
+    });
+
+    res.send(imageList);
+  });
 });
 
 console.log('Server up on port ' + portNum + '.');
