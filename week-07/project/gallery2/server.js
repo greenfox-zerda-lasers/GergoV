3f25ebbdb2cce422cbac0e79f29cc52af18092eb
geritwo <geritwo@gmail.com>
2017-03-16 16:53:30 +0100

    Image parsing part broken. TODO: Clarify cause of error, debug.

--- week-07/project/gallery2/server.js
+++ week-07/project/gallery2/server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
-const ExifImage = require('exif').ExifImage;
+const imageMeta = require('./_data/data.json');
 
 // Express config
 const app = express();
@@ -16,7 +16,9 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/app'));
 app.use('/_data', express.static(__dirname + '/_data'));
 
+// Gallery image related data
 const p = './_data/';
+console.log(imageMeta[0].title);
 // Endpoints
 app.get('/files', function(req, res) {
 
@@ -28,22 +30,15 @@ app.get('/files', function(req, res) {
       throw err;
     }
 
-    files.map(function addPath(file) {
+    let imageBatch = files.map(function addPath(file) {
       return path.join(p, file); // Add path to filenames
     }).filter(function filterFiles(file) {
       return fs.statSync(file).isFile(); // Return files only
-    }).forEach(function compileArray(file, index) {
-      try {
-          new ExifImage({ image : file }, function (error, exifData) {
-              if (error)
-                  console.log('Error: '+error.message);
-              else
-                  console.log(exifData); // Do something with your data!
-          });
-      } catch (error) {
-          console.log('Error: ' + error.message);
-      }
-      imageList.push({ "url":file, "metadata":ExifImage.exifData, }); // Compile list of files in array
+    })
+
+    imageBatch.forEach(function compileArray(file, index) {
+      console.log(file, index);
+      imageList.push({ "url":file, "title": imageMeta[index].title, "description": imageMeta[index].description }); // Compile list of files in array
     });
 
     res.send(imageList);
